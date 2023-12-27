6b48d65c49535cdb40fc25e9b30844b8d87306af
liqiang <liqiang@makeblock.com>
2023-12-27 18:38:47 +0800

    watch 监听

--- src/components/CompositionPerson.vue
+++ src/components/CompositionPerson.vue
@@ -2,37 +2,43 @@
 
 <template>
   <div class="person">
-    <h2>姓名: {{name}}</h2>
-    <h2>年龄: {{age}}</h2>
+    <h1>监听 ref 定义的对象类型数据</h1>
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
     <button @click="changeName">修改名字</button>
     <button @click="changeAge">修改年龄</button>
+    <button @click="changePerson">修改整个人</button>
   </div>
 </template>
 
 <script lang="ts" name='Person456' setup>
-  import { reactive, ref, toRef, toRefs } from 'vue';
+  import {ref, watch} from 'vue'
 
-  let person = reactive(
-    {
-      name: '张三',
-      age: 18,
-    }
-  );
-
-  let {name, age} = toRefs(person);
-  let n1 = toRef(person, 'age');
-  console.log(n1.value);
+  // 数据
+  let person = ref({
+    name: '张三',
+    age: 18,
+  });
 
   // 方法
   function changeName(){
-    name.value = 'zhang-san'
-    console.log(name.value, person.name);
+    person.value.name += '~'
   }
-
   function changeAge(){
-    age.value += 1;
+    person.value.age += 1
+  }
+  function changePerson(){
+    person.value = {name: '李四', age:90}
   }
 
+  // 监视的是对象的地址值，若想监视对象的内部属性变化，需要手动开启深度监视
+  // watch 的第一个参数是：被监视的数据
+  // watch 的第二个参数是：被监视的回调
+  // watch 的第三个参数是：配置对象(deep, immediate等等)
+  watch(person, (newValue, oldValue) => {
+    console.log('person 变化了', newValue, oldValue)
+  }, {deep: true, immediate: true});
+  
 </script>
 
 <style scoped>
