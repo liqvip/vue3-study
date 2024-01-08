83d68e27bbe4fc9a230f85cf8a6580526f58550e
liqiang <liqiang@makeblock.com>
2024-01-08 17:19:54 +0800

    props 组件通信

--- src/pages/01_props/Child.vue
+++ src/pages/01_props/Child.vue
@@ -1,6 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <h3>子组件</h3>
+    <h4>玩具：{{ toy }}</h4>
+    <h4>父给的车：{{ car }}</h4>
+    <button @click="sendToy(toy)">把玩具给父亲</button>
+  </div>
 </template>
 
 <script setup lang="ts" name="">
+  import { ref } from 'vue';
+  // 数据
+  let toy = ref('奥特曼')
+
+  // 声明接收 props
+  defineProps(['car', 'sendToy'])
 </script>
--- src/pages/01_props/Father.vue
+++ src/pages/01_props/Father.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>fdfdf</div>
+  <div>
+    <h3>父组件</h3>
+    <h4>汽车：{{ car }}</h4>
+    <h4 v-show="toy">子给的玩具：{{ toy }}</h4>
+    <Child :car="car" :sendToy="getToy"/>
+  </div>
 </template>
 
 <script setup lang="ts">
+  import { ref } from 'vue';
+  import Child from './Child.vue'
+
+  // 数据
+  let car = ref('奔驰')
+  let toy = ref('')
+
+  // 方法
+  function getToy(value: string){
+    console.log('父', value)
+    toy.value = value
+  }
 </script>
