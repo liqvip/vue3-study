a698224d3a4e2eae35818d45ef3edda0fd73a491
liqiang <liqiang@makeblock.com>
2024-07-27 15:35:57 +0800

    测试页面

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
-    <Home/>
+    <Test/>
+<!--    <Home/>-->
   </div>
 </template>
 
 <script lang="ts" name="App" setup>
   import Home from '@/pages/Home.vue'
+  import Test from '@/pages/Test.vue'
 </script>
---
+++ src/pages/Test.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts"> import { ref, watch } from 'vue'
+  const person = ref({
+    name: '张三',
+    age: 18,
+  });
+
+  function changeName() {
+    person.value.name = '李四';
+  }
+
+  function changePerson() {
+    person.value = {
+      name: '王五',
+      age: 20,
+    };
+  }
+
+  watch(() => person.value.name, (newVal, oldVal) => {
+    console.log('person 改变了',newVal, oldVal);
+  });
+</script>
+
+<template>
+  <div>
+    <h3>姓名：{{ person.name }}</h3>
+    <h3>年龄：{{ person.age }}</h3>
+    <button @click="changeName">改名</button>
+    <button @click="changePerson">改人</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
